beb3484ebc8cab7fdb43325f1088fbdbb472da03
felix <felix@hallerbach.info>
2023-12-15 12:33:23 +0100

    initial commit

---
+++ src/main/java/de/flix29/Main.java
@@ -0,0 +1,10 @@
+package de.flix29;
+
+import de.flix29.javaUni.Maps;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Maps maps = new Maps();
+    }
+}
---
+++ src/main/java/de/flix29/javaUni/Maps.java
@@ -0,0 +1,27 @@
+package de.flix29.javaUni;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author felix
+ */
+public class Maps {
+
+    public Maps() {
+
+    }
+
+    /**
+     *
+     */
+    public void initalizeMaps() {
+        //initialize a Map as a hashMap
+        Map<?, ?> hashMap = new HashMap<>();
+
+        //initialize a Map as a treeMap
+        Map<?, ?> treeMap = new TreeMap<>();
+    }
+}
