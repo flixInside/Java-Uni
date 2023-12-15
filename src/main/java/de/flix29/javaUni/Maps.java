a839236cb260adf4a8f7693ba39e4b7ef7fd0812
felix <felix@hallerbach.info>
2023-12-15 15:03:06 +0100

    added docu to map class

--- src/main/java/de/flix29/javaUni/Maps.java
+++ src/main/java/de/flix29/javaUni/Maps.java
@@ -5,6 +5,10 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
+ * Diese Klasse zeigte verschiedene Funktionalitäten der Klasse {@link Map}
+ * <ul>
+ *     <li>{@link #initalizeMaps()} initialisierung von Maps</li>
+ * </ul>
  *
  * @author felix
  */
@@ -15,7 +19,11 @@ public class Maps {
     }
 
     /**
-     *
+     * Diese Methode zeigt die verschiedenen Möglichkeiten eine Map zu initialisieren
+     * <ul>
+     *     <li>Die erste Möglichkeit ist sie als {@link HashMap} zu initialisieren</li>
+     *     <li>Die zweite Möglichkeit ist sie als {@link TreeMap} zu initialisieren</li>
+     * </ul>
      */
     public void initalizeMaps() {
         //initialize a Map as a hashMap
